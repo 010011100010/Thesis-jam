cfe75ffc7d48e5516dffbf5fd2dda1b38a43eed2
010011100010 <ll3305@nyu.edu>
2017-09-03 14:13:44 -0400

    Some additions
    Fixed hammer pivot point;
    Added hammer SFX.

---
+++ Knock_Knock/Assets/Assets/Scripts/hammer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hammer : MonoBehaviour {
+	public List<AudioClip> hammerSounds;
+	AudioSource hammerSounding;
+	// Use this for initialization
+	void Start () {
+		hammerSounding = this.gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision hammerCollision){
+		if (hammerCollision.gameObject.CompareTag ("Metal")) {
+			hammerSounding.clip = hammerSounds [0];
+			hammerSounding.Play ();
+		}
+
+		if (hammerCollision.gameObject.CompareTag ("Wood")) {
+			hammerSounding.clip = hammerSounds [1];
+			hammerSounding.Play ();
+		}
+	}
+}
