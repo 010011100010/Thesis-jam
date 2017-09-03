447b317f8093c3b4aeac6c25c8451d300dda10e8
David Frisch <dmf463@nyu.edu>
2017-09-03 11:45:13 -0400

    All chairs will play SFX on collision

---
+++ Knock_Knock/Assets/Assets/Scripts/soundOnColl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundOnColl : MonoBehaviour {
+    public List<AudioClip> Sounds;
+    AudioSource Sounding;
+    // Use this for initialization
+    void Start () {
+        Sounding = this.gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter()
+    {
+        Sounding.clip = Sounds[Random.Range(0, Sounds.Count-1)];
+        Sounding.Play();
+    }
+}
